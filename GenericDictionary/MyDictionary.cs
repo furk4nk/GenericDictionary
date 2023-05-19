00b0e3232f831888bbba90e24e371dae38fd9cbf
furk4nk <95353888+furk4nk@users.noreply.github.com>
2023-05-19 20:25:21 +0300

    Add project files.

---
+++ GenericDictionary/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDictionary
+{
+    public class MyDictionary<Tkey, Tvalue>
+    {
+        private Tkey[] _keyArray;
+        private Tvalue[] _valueArray;
+
+        private Tkey[] tempKeyArray;
+        private Tvalue[] tempValueArray;
+        public MyDictionary()
+        {
+            _keyArray = new Tkey[0];
+            _valueArray = new Tvalue[0];
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            tempKeyArray = _keyArray;
+            tempValueArray = _valueArray;
+
+            _keyArray = new Tkey[_keyArray.Length +1];
+            _valueArray = new Tvalue[_valueArray.Length +1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                _keyArray[i] = tempKeyArray[i];
+                _valueArray[i] = tempValueArray[i];
+            }
+
+            _keyArray[^1] = key;
+            _valueArray[^1] = value;
+        }
+
+        public Tkey[] Keys
+        {
+            get { return _keyArray; }
+        }
+        public Tvalue[] Values
+        {
+            get { return _valueArray; }
+        }
+    }
+}
---
+++ GenericDictionary/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using GenericDictionary;
+
+MyDictionary<string,int> list = new();
+list.Add("ankara", 0);
+list.Add("istanbul", 1);
+Console.WriteLine(list.Values[0]);
+
